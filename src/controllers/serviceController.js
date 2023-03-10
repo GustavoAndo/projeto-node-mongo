a59c0a3cee411aed75f64e45403d7c47c132d8c3
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-03-10 16:39:08 -0300

    Funcionalidade de Atualizar o Usuário

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -38,7 +38,7 @@ const serviceController = {
                 return
             }
 
-            const service = await Service.findOne({_id: id})
+            const service = await Service.findById(id)
               
             if (!service) {
                 res.status(404).json({ msg: "Serviço não encontrado." })
@@ -67,14 +67,44 @@ const serviceController = {
                 return
             }
 
-            const deletedService = await Service.findOneAndDelete({_id: id})
+            const deletedService = await Service.findByIdAndDelete(id)
 
             res.status(200).json({deletedService, msg: "Serviço excluído com sucesso"})
         } catch (error) { 
             res.status(500).json({error})
         }
-    }
+    },
 
+    update: async(req, res) => {
+        try {
+            const id = req.params.id
+
+            if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+                res.status(404).json({ msg: "Id inválido." })
+                return
+            }
+    
+            const { name, description, price, image } = req.body
+    
+            const service = {
+                name,
+                description,
+                price,
+                image
+            }
+    
+            const updatedService = await Service.findByIdAndUpdate(id, service)
+    
+            if (!updatedService) {
+                res.status(404).json({msg: "Serviço não encontrado"})
+                return
+            } 
+    
+            res.status(200).json({service, msg: "Serviço atualizado com sucesso."})
+        } catch (error) {
+            res.status(500).json({error})
+        }
+    }
 }
 
 module.exports = serviceController
--- src/routes/services.js
+++ src/routes/services.js
@@ -3,8 +3,13 @@ const router = require("express").Router()
 const serviceController = require("../controllers/serviceController")
 
 router.route("/").post((req, res) => serviceController.create(req, res))
+
 router.route("/").get((req, res) => serviceController.getAll(req, res))
+
 router.route("/:id").get((req, res) => serviceController.get(req, res))
+
 router.route("/:id").delete((req, res) => serviceController.delete(req, res))
+
+router.route("/:id").patch((req, res) => serviceController.update(req, res))
 
 module.exports = router
