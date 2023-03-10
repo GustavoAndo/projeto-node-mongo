f29c4cf9a7911196811e08e56a74fa54d36e290c
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-03-10 16:03:45 -0300

    Funcionalidade de pegar um serviço por id

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -27,7 +27,29 @@ const serviceController = {
         } catch (error) {
             res.status(500).json({error: error})
         }
-    }
+    },
+
+    get: async(req, res) => {
+        try {
+            const id = req.params.id
+            
+            if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+                res.status(404).json({ msg: "Id inválido." })
+                return
+            }
+
+            const service = await Service.findOne({_id: id})
+              
+            if (!service) {
+                res.status(404).json({ msg: "Serviço não encontrado." })
+                return
+            }
+
+            res.json(service)
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
+    },
 
 }
 
--- src/routes/services.js
+++ src/routes/services.js
@@ -4,5 +4,6 @@ const serviceController = require("../controllers/serviceController")
 
 router.route("/").post((req, res) => serviceController.create(req, res))
 router.route("/").get((req, res) => serviceController.getAll(req, res))
+router.route("/:id").get((req, res) => serviceController.get(req, res))
 
 module.exports = router
