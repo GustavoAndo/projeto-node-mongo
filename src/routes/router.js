2c727a581e37963f51d36266b3a371e917f0a523
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-03-11 14:52:27 -0300

    Criando authmiddleware de verificação de autenticação

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,11 @@
 const router = require("express").Router()
+const checkToken = require("../middlewares/authMiddleware")
 
 const servicesRouter = require("./services")
 router.use("/service", servicesRouter)
 
 const partiesRouter = require("./parties")
-router.use("/party", partiesRouter)
+router.use("/party", checkToken, partiesRouter)
 
 const usersRouter = require("./users")
 router.use("/user", usersRouter)
